3250bb546e8d27c6040ee290fb56b38d3a1fd4c4
medanisjbara <anis2834133766619@gmail.com>
2022-03-04 01:17:43 +0100

    Troubleshooted cuda_runtime

--- cpyrit/setup.py
+++ cpyrit/setup.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python
 from distutils.core import setup, Extension
 
 cmodule = Extension('_cpyrit',
-                    libraries = ['cuda','cudart','ssl'],
+                    libraries = ['cuda','ssl'],
                     sources = ['cpyrit.c'],
-                    extra_compile_args = ['-O0','-ggdb'],
-                    include_dirs = ['/usr/local/cuda/include'],
+                    extra_compile_args = ['-O0','-ggdb','-I/opt/cuda/targets/x86_64-linux/include'],
+                    include_dirs = ['/opt/cuda/targets/x86_64-linux/include','/opt/cuda/targets/x86_64-linux/lib'],
                     extra_objects = ['cpyrit_cuda.o']
                     )
 
