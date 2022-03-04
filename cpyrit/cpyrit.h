ac5e796411b3e6840338d29efd1337da504bf4e3
medanisjbara <anis2834133766619@gmail.com>
2022-03-04 01:34:39 +0100

    Removed licence (this program is not licenced (hopefully this isn't illegal)

--- cpyrit/cpyrit.h
+++ cpyrit/cpyrit.h
@@ -1,18 +1,6 @@
 /*
-#    This file is part of Pyrit.
+#    This file is part of Pyrit2 an attempt to rewrite Pyrit in python3.
 #
-#    Pyrit is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Pyrit is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Pyrit.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef CPYRIT
